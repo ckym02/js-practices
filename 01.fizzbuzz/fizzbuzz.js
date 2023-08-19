a5afde5f2faaf5e20003b603df97ffa1ec6e38e7
koyama-c <koyama@linkedge.jp>
2023-08-20 01:13:10 +0900

    elseを使って条件分岐する

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,16 +2,12 @@
 
 for (let i = 1; i <= 20; i++) {
   if (i % 15 === 0) {
-    console.log('FizzBuzz');
-    continue;
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-  if (i % 3 === 0) {
-    console.log('Fizz');
-    continue;
-  }
-  if (i % 5 === 0) {
-    console.log('Buzz');
-    continue;
-  }
-  console.log(i);
 }
