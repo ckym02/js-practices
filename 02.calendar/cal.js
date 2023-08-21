62575bc103602db46ed9f8a61d00b6ec0ee46418
koyama-c <koyama@linkedge.jp>
2023-08-21 22:21:06 +0900

    変数名の変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,19 +1,6 @@
 import minimist from "minimist";
 import dayjs from "dayjs";
 
-function cal(targetDate) {
-  let printDate = "";
-  const lastDate = targetDate.endOf("month").date();
-  for (let date = 1; date <= lastDate; date++) {
-    const day = targetDate.date(date).day();
-    // 曜日が土曜日(6)であれば改行する、それ以外は半角スペースを末尾に追加する
-    const appendDate =
-      day === 6 ? `${(" " + date).slice(-2)}\n` : `${(" " + date).slice(-2)} `;
-    printDate = printDate + appendDate;
-  }
-  return printDate;
-}
-
 function main() {
   const argv = minimist(process.argv.slice(2));
 
@@ -22,10 +9,20 @@ function main() {
 
   // new Date()の月は、0(1月)~11(12月)の値で指定する
   const targetDate = dayjs(new Date(targetYear, targetMonth - 1));
+  const lastMonthDate = targetDate.endOf("month").date();
 
-  const firstDay = targetDate.startOf("month").day();
-  const space = "   ".repeat(firstDay);
-  console.log(`        ${targetYear}\n` + space + cal(targetDate));
+  let printDate = "";
+  for (let date = 1; date <= lastMonthDate; date++) {
+    const day = targetDate.date(date).day();
+    // 曜日が土曜日(6)であれば改行する、それ以外は半角スペースを末尾に追加する
+    const appendDate =
+      day === 6 ? `${(" " + date).slice(-2)}\n` : `${(" " + date).slice(-2)} `;
+    printDate = printDate + appendDate;
+  }
+
+  const firstMonthDay = targetDate.startOf("month").day();
+  const space = "   ".repeat(firstMonthDay);
+  process.stdout.write(`        ${targetYear}\n${space}${printDate}`);
 }
 
 main();
