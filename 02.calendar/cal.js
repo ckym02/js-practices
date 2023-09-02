04992cfd6ccf30c214f9f8652d8eb22167a5875a
koyama-c <koyama@linkedge.jp>
2023-09-02 16:01:25 +0900

    dayjsオブジェクトの生成箇所を変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,10 +1,7 @@
 import minimist from "minimist";
 import dayjs from "dayjs";
 
-function printDate(selectedMonthObject) {
-  const endOfMonthObject = selectedMonthObject.endOf("month");
-  const startOfMonthObject = selectedMonthObject.startOf("month");
-
+function printDate(startOfMonthObject, endOfMonthObject) {
   for (
     let object = startOfMonthObject;
     object.isBefore(endOfMonthObject);
@@ -31,13 +28,16 @@ function printCalendar() {
   const targetYear = argv.y ?? today.year();
 
   // new Date()の月は、0(1月)~11(12月)の値で指定する
-  const selectedMonthObject = dayjs(new Date(targetYear, targetMonth - 1));
-
-  const startDayOfWeekOfMonth = selectedMonthObject.startOf("month").day();
+  const startOfMonthObject = dayjs(
+    new Date(targetYear, targetMonth - 1)
+  ).startOf("month");
+  const endOfMonthObject = dayjs(new Date(targetYear, targetMonth - 1)).endOf(
+    "month"
+  );
 
   process.stdout.write(`      ${targetMonth}月 ${targetYear}\n`);
-  process.stdout.write("   ".repeat(startDayOfWeekOfMonth));
-  printDate(selectedMonthObject);
+  process.stdout.write("   ".repeat(startOfMonthObject.day()));
+  printDate(startOfMonthObject, endOfMonthObject);
 }
 
 printCalendar();
