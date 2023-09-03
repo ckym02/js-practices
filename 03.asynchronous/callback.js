94a51221f675347669a7ea5ea04178e41a5d6fbc
koyama-c <koyama@linkedge.jp>
2023-09-03 13:35:18 +0900

    callbackを使う

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,29 @@
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+const db = new sqlite3.Database(":memory:");
+
+db.run("CREATE TABLE books (title text not null unique)", function () {
+  db.run("INSERT INTO books VALUES ('ほんのなまえ')", function () {
+    console.log(`ID: ${this.lastID}`);
+
+    db.get("SELECT rowid AS id, title FROM books", function (err, row) {
+      console.log(`ID: ${row.id}, title: ${row.title}`);
+
+      db.run("DROP TABLE books");
+    });
+  });
+});
+
+await timers.setTimeout(100);
+
+db.run("CREATE TABLE books (title text not null unique)", function () {
+  db.run("INSERT INTO books VALUES (NULL)", function (err) {
+    console.error(`レコード追加に失敗しました。${err.message}`);
+
+    db.get("SELECT rowid AS id, hoge FROM books", function (err) {
+      console.error(`レコードの取得に失敗しました。${err.message}`);
+
+      db.run("DROP TABLE books");
+    });
+  });
+});
