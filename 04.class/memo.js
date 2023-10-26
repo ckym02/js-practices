0856c1b0a8204611af6b3ea75e3efa8dee50d911
koyama-c <koyama@linkedge.jp>
2023-10-26 22:14:12 +0900

    排他的な処理になるように修正する

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -6,64 +6,73 @@ import Memo from "./class/memo.js";
 
 const [, , option] = process.argv;
 
-if (option === "-l") {
-  const memo = new Memo();
-  const memos = await memo.selectAll();
-  memos.forEach((memo) => {
-    console.log(memo.content.split(/\n/)[0]);
-  });
-}
+switch (option) {
+  case "-l": {
+    const memo = new Memo();
+    const memos = await memo.selectAll();
+    memos.forEach((memo) => {
+      console.log(memo.content.split(/\n/)[0]);
+    });
+    break;
+  }
 
-if (option === "-r") {
-  const memo = new Memo();
-  const memos = await memo.selectAll();
-  const choices = memos.map((memo) => {
-    return { name: memo.content.split(/\n/)[0], value: memo.content };
-  });
-  const question = {
-    message: "Choose a memo you want to see:",
-    choices: choices,
-    result(names) {
-      return this.map(names);
-    },
-  };
+  case "-r": {
+    const memo = new Memo();
+    const memos = await memo.selectAll();
+    const choices = memos.map((memo) => {
+      return { name: memo.content.split(/\n/)[0], value: memo.content };
+    });
+    const question = {
+      message: "Choose a memo you want to see:",
+      choices: choices,
+      result(names) {
+        return this.map(names);
+      },
+    };
+    const prompt = new Enquirer.Select(question);
+    const answer = await prompt.run();
+    console.log(Object.values(answer)[0]);
+    break;
+  }
 
-  const prompt = new Enquirer.Select(question);
-  const answer = await prompt.run();
-  console.log(Object.values(answer)[0]);
-}
+  case "-d": {
+    const memo = new Memo();
+    const memos = await memo.selectAll();
+    const choices = memos.map((memo) => {
+      return { name: memo.content.split(/\n/)[0], value: memo.id };
+    });
+    const question = {
+      message: "Choose a memo you want to delete:",
+      choices: choices,
+      result(names) {
+        return this.map(names);
+      },
+    };
+    const prompt = new Enquirer.Select(question);
+    const answer = await prompt.run();
+    memo.delete(Object.values(answer)[0]);
+    break;
+  }
 
-if (option === "-d") {
-  const memo = new Memo();
-  const memos = await memo.selectAll();
-  const choices = memos.map((memo) => {
-    return { name: memo.content.split(/\n/)[0], value: memo.id };
-  });
-  const question = {
-    message: "Choose a memo you want to delete:",
-    choices: choices,
-    result(names) {
-      return this.map(names);
-    },
-  };
+  case undefined: {
+    const reader = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  const prompt = new Enquirer.Select(question);
-  const answer = await prompt.run();
-  memo.delete(Object.values(answer)[0]);
-}
+    let lines = [];
+    reader.on("line", (input) => {
+      lines.push(input);
+    });
+    reader.on("close", () => {
+      const memo = new Memo(lines.join("\n"));
+      memo.create();
+    });
+    break;
+  }
 
-if (!option) {
-  const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  let lines = [];
-  reader.on("line", (input) => {
-    lines.push(input);
-  });
-  reader.on("close", () => {
-    const memo = new Memo(lines.join("\n"));
-    memo.create();
-  });
+  default: {
+    console.log("Empty action received.");
+    break;
+  }
 }
