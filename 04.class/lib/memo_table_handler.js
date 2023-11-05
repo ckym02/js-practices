22bae78568bb832a20b43746ead68fb3fb364a49
koyama-c <koyama@linkedge.jp>
2023-11-05 17:48:58 +0900

    クラス外部から呼ばれないためプライベートにする

--- 04.class/lib/memo_table_handler.js
+++ 04.class/lib/memo_table_handler.js
@@ -5,7 +5,7 @@ export default class MemoTableHandler {
     this.sqlite = new Sqlite("memos_table.db");
   }
 
-  static async build() {
+  static async #build() {
     const memoTableHandler = new MemoTableHandler();
     await memoTableHandler.#createTable();
     return memoTableHandler;
@@ -18,21 +18,21 @@ export default class MemoTableHandler {
   }
 
   static async create(content) {
-    const memoTableHandler = await this.build();
+    const memoTableHandler = await this.#build();
     memoTableHandler.sqlite.run(
       `INSERT INTO memos (content) VALUES ('${content}')`
     );
   }
 
   static async selectAll() {
-    const memoTableHandler = await this.build();
+    const memoTableHandler = await this.#build();
     return memoTableHandler.sqlite.all(
       "SELECT rowid AS id, content FROM memos ORDER BY rowid"
     );
   }
 
   static async delete(id) {
-    const memoTableHandler = await this.build();
+    const memoTableHandler = await this.#build();
     memoTableHandler.sqlite.run(`DELETE FROM memos WHERE rowid = ${id}`);
   }
 }
